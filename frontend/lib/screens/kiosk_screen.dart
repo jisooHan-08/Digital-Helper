ed579a00b5e30cd97a5a4809b67e7bd8e1a8d50a
shin heewon <gml5982@naver.com>
2025-07-05 03:34:27 +0900

    front file

---
+++ frontend/lib/screens/kiosk_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class KioskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('키오스크 연습')),
+      body: Center(child: Text('여기에 키오스크 기능이 들어갈 거예요')),
+    );
+  }
+}
